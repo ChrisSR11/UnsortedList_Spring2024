16ae4599803f3b5447753fb03bf106d8a6bda3d0
Christopher Soto Reyes <csotoreyes@horizon.csueastbay.edu>
2024-01-30 11:18:08 -0800

    Complete w/ Template

--- UnsortedList.cpp
+++ UnsortedList.cpp
@@ -1,24 +1,28 @@
-#Christopher_Soto_Reyes
 #include "UnsortedList.h"
 
-UnsortedList::UnsortedList() {
+template<class T>
+UnsortedList<T>::UnsortedList() {
     length = 0;
     currentPos = 0;
 }
 
-void UnsortedList::MakeEmpty() {
+template<class T>
+void UnsortedList<T>::MakeEmpty() {
     length = 0;
 }
 
-bool UnsortedList::IsFull() const {
+template<class T>
+bool UnsortedList<T>::IsFull() const {
     return length == MAX_ITEMS;
 }
 
-int UnsortedList::GetLength() const {
+template<class T>
+int UnsortedList<T>::GetLength() const {
     return length;
 }
 
-bool UnsortedList::Contains(int someItem) {
+template<class T>
+bool UnsortedList<T>::Contains(T someItem) {
     for(int i = 0; i < length; i++)
     {
         if(arr[i] == someItem)
@@ -28,12 +32,14 @@ bool UnsortedList::Contains(int someItem) {
     return false;
 }
 
-void UnsortedList::AddItem(int item) {
+template<class T>
+void UnsortedList<T>::AddItem(T item) {
     if(!IsFull())
         arr[length++] = item;
 }
 
-void UnsortedList::DeleteItem(int item) {
+template<class T>
+void UnsortedList<T>::DeleteItem(T item) {
     for(int i = 0; i < length; i++)
     {
         if(arr[i] == item)
@@ -43,11 +49,13 @@ void UnsortedList::DeleteItem(int item) {
     }
 }
 
-void UnsortedList::ResetIterator() {
+template<class T>
+void UnsortedList<T>::ResetIterator() {
     currentPos = 0;
 }
 
-int UnsortedList::GetNextItem() {
+template<class T>
+T UnsortedList<T>::GetNextItem() {
     return arr[currentPos++];
 }
 
